eeff0573445986f999c11a4746f369bced2096f3
Byungchul Park <be.assiduous@gmail.com>
2023-01-09 02:50:36 +0900

    keeper methods for module parameters

--- x/amm/keeper/params.go
+++ x/amm/keeper/params.go
@@ -1,6 +1,7 @@
 package keeper
 
 import (
+	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/fates1046/chaos/x/amm/types"
 )
@@ -14,4 +15,16 @@ func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
 // SetParams set the params
 func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 	k.paramSpace.SetParamSet(ctx, &params)
+}
+
+// GetFeeRate returns the FeeRate param
+func (k Keeper) GetFeeRate(ctx sdk.Context) (feeRate sdk.Dec) {
+	k.paramSpace.Get(ctx, types.KeyFeeRate, &feeRate)
+	return
+}
+
+// GetMinInitialLiquidity returns the MinInitialLiquidity param
+func (k Keeper) GetMinInitialLiquidity(ctx sdk.Context) (minInitialLiquidity math.Int) {
+	k.paramSpace.Get(ctx, types.KeyMinInitialLiquidity, &minInitialLiquidity)
+	return
 }
