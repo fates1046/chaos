0474f4325e00e6f886ea65dddc4764f1a5dcab0c
Byungchul Park <be.assiduous@gmail.com>
2023-01-09 00:46:23 +0900

    Message handler

---
+++ x/amm/keeper/msg_server.go
@@ -0,0 +1,17 @@
+package keeper
+
+import (
+	"github.com/fates1046/chaos/x/amm/types"
+)
+
+var _ types.MsgServer = msgServer{}
+
+type msgServer struct {
+	Keeper
+}
+
+// NewMsgServerImpl returns an implementation of the MsgServer interface
+// for the provided Keeper.
+func NewMsgServerImpl(keeper Keeper) types.MsgServer {
+	return &msgServer{Keeper: keeper}
+}
