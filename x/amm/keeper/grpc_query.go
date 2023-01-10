b8dceddc0443b029ba49c260d6b298741da65181
Byungchul Park <be.assiduous@gmail.com>
2023-01-11 00:46:30 +0900

    query service implementation for pair information

--- x/amm/keeper/grpc_query.go
+++ x/amm/keeper/grpc_query.go
@@ -2,7 +2,9 @@ package keeper
 
 import (
 	"context"
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/fates1046/chaos/x/amm/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -25,4 +27,33 @@ func (k Querier) Params(c context.Context, req *types.QueryParamsRequest) (*type
 	ctx := sdk.UnwrapSDKContext(c)
 
 	return &types.QueryParamsResponse{Params: k.GetParams(ctx)}, nil
+}
+
+func (k Querier) Pairs(c context.Context, req *types.QueryPairsRequest) (*types.QueryPairsResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(c)
+
+	store := ctx.KVStore(k.storeKey)
+	planStore := prefix.NewStore(store, types.PairKeyPrefix)
+	var pairs []types.Pair
+	pageRes, err := query.Paginate(planStore, req.Pagination, func(key, value []byte) error {
+		var pair types.Pair
+		if err := k.cdc.Unmarshal(value, &pair); err != nil {
+			return err
+		}
+		pairs = append(pairs, pair)
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryPairsResponse{
+		Pairs:      pairs,
+		Pagination: pageRes,
+	}, nil
 }
