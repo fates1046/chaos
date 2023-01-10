497d146a2fce5a48fd4631c01e47a33651436388
Byungchul Park <be.assiduous@gmail.com>
2023-01-10 20:02:17 +0900

    message server implementation for add liquidity

--- x/amm/keeper/msg_server.go
+++ x/amm/keeper/msg_server.go
@@ -1,6 +1,8 @@
 package keeper
 
 import (
+	"context"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/fates1046/chaos/x/amm/types"
 )
 
@@ -8,6 +10,21 @@ var _ types.MsgServer = msgServer{}
 
 type msgServer struct {
 	Keeper
+}
+
+func (m msgServer) AddLiquidity(c context.Context, msg *types.MsgAddLiquidity) (*types.MsgAddLiquidityResponse, error) {
+	ctx := sdk.UnwrapSDKContext(c)
+
+	mintedShare, err := m.Keeper.AddLiquidity(
+		ctx, sdk.MustAccAddressFromBech32(msg.Sender), msg.Coins,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &types.MsgAddLiquidityResponse{
+		MintedShare: mintedShare,
+	}, nil
+
 }
 
 // NewMsgServerImpl returns an implementation of the MsgServer interface
